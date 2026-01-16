beec8614da320319d95dbd3b46a2abb399f00be3
FLawrence <faith.lawrence@nationalarchives.gov.uk>
2026-01-16 16:22:28 +0000

    Rejig code to make it easier to call from other places if needed

--- enrichment_extraction.py
+++ enrichment_extraction.py
@@ -7,23 +7,29 @@ import pandas as pd
 # Extract marked up references within the XML
 # Output CSV of extracted values
 
-base_folder = "C:/Users/flawrence/Documents/Projects/FCL/Research Area/ds-caselaw-blackstone-analysis/"
-data_base_folder = "C:/Users/flawrence/Documents/Projects/FCL/Research Area/ds-caselaw-data-analysis/"
+def extract_references(base_folder, xml_folder):
+    transformation_file = Path(base_folder, "data/xslt/getReferenceValues.xsl")
+    cache_folder = Path(base_folder,"cache")
 
-xml_folder = [Path(data_base_folder, "data/xml-enriched-bucket-test/"), Path(data_base_folder, "data/xml-second-phase-enriched-bucket-test/"), Path(data_base_folder, "data/xml-third-phase-enriched-bucket-test/")]
-transformation_file = Path(base_folder, "data/xslt/getReferenceValues.xsl")
-cache_folder = Path(base_folder,"cache")
+    #judgment_data = xf.get_data_from_transform(xml_folder, transformation_file, filter=["eat-2022-1.xml", "eat-2022-3.xml"])
+    judgment_data = xf.get_data_from_transform(xml_folder, transformation_file)
+    print(judgment_data)
 
-#judgment_data = xf.get_data_from_transform(xml_folder, transformation_file, filter=["eat-2022-1.xml", "eat-2022-3.xml"])
-judgment_data = xf.get_data_from_transform(xml_folder, transformation_file)
-print(judgment_data)
+    #print(judgment_data['filename'].nunique())
 
-#print(judgment_data['filename'].nunique())
+    if not(judgment_data.empty):
 
-if not(judgment_data.empty):
+        for id, group in judgment_data.groupby('filename'):
+            #print(id)
+            #print(group)
+            group.to_csv(Path(cache_folder, id + "_enrichmented_refs.csv"), index=False)
+            print("Saving csv to " + str(Path(cache_folder, id + "_enrichmented_refs.csv")))
 
-    for id, group in judgment_data.groupby('filename'):
-        #print(id)
-        #print(group)
-        group.to_csv(Path(cache_folder, id + "_enrichmented_refs.csv"), index=False)
-        print("Saving csv to " + str(Path(cache_folder, id + "_enrichmented_refs.csv")))
+
+if __name__ == '__main__':
+    base_folder = "C:/Users/flawrence/Documents/Projects/FCL/Research Area/ds-caselaw-blackstone-analysis/"
+    data_base_folder = "C:/Users/flawrence/Documents/Projects/FCL/Research Area/ds-caselaw-data-analysis/"
+    xml_folder = [Path(data_base_folder, "data/xml-enriched-bucket-test/"), Path(data_base_folder, "data/xml-second-phase-enriched-bucket-test/"), Path(data_base_folder, "data/xml-third-phase-enriched-bucket-test/")]
+
+
+    extract_references(base_folder, xml_folder)
